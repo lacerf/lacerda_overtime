525a0c7be11ef978abed2b367a6d0c3b345fbc3a
Fabio Lacerda <lacerf@gmail.com>
2017-05-05 22:28:48 +0000

    update seed and implemented the posts index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show]
+	before_action :set_post, only: [:show]
+	
 	def index
+		@posts = Post.all
 	end
 
 	def new
@@ -30,5 +32,4 @@ class PostsController < ApplicationController
 	  def set_post
 	  	@post = Post.find(params[:id])
 	  end
-	  
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,16 +1,30 @@
 require 'rails_helper'
 
 describe 'navigate' do
-    describe 'index' do
+  before do
+    user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+    login_as(user, :scope => :user)
+  end
+
+  describe 'index' do
+    before do
+      visit posts_path
+    end
+
   	it 'can be reached successfully' do
-  		visit posts_path
   		expect(page.status_code).to eq(200)
   	end
 
   	it 'has a title of Posts' do
-  		visit posts_path
   		expect(page).to have_content(/Posts/)
   	end
+
+    it 'has a list of posts' do
+      post1 = Post.create(date: Date.today, rationale: "Post1")
+      post2 = Post.create(date: Date.today, rationale: "Post2")
+      visit posts_path
+      expect(page).to have_content(/Post1|Post2/)
+    end
   end
 
   describe 'creation' do
@@ -29,6 +43,13 @@ describe 'navigate' do
 
       expect(page).to have_content("Some rationale")
   	end
+
+    it 'will have a user associated it' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "User Association"
+      click_on "Save"
+
+      expect(User.last.posts.last.rationale).to eq("User Association")
+    end
   end
-  
 end

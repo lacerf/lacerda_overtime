aba44e5ab81508b34c67102bb039425f4f54d629
Fabio Lacerda <lacerf@gmail.com>
2017-05-06 01:00:55 +0000

    Implemented the edit and update functionality for posts

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,3 @@
-
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 
@@ -22,4 +21,5 @@ RSpec.configure do |config|
   config.after(:each) { DatabaseCleaner.clean }
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.include FactoryGirl::Syntax::Methods
 end

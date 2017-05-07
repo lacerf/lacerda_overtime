3d5ff894cfc82e1c19e37204dcfd20e1948c1a67
Fabio Lacerda <lacerf@gmail.com>
2017-05-07 12:00:24 +0000

    Updated the audit log factory and the seed file to create audit logs

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@
 										password_confirmation: "asdfasdf",
 										first_name: "Jon",
 										last_name: "Snow",
-										phone: "3014668742")
+										phone: "4322386131")
 
 puts "1 User created"
 
@@ -12,7 +12,7 @@ AdminUser.create(email: "admin@test.com",
 									password_confirmation: "asdfasdf",
 									first_name: "Admin",
 									last_name: "Name",
-									phone: "3014668742")
+									phone: "4322386131")
 
 puts "1 Admin User created"
 
@@ -21,3 +21,9 @@ puts "1 Admin User created"
 end
 
 puts "100 Posts have been created"
+
+100.times do |audit_log|
+	AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+end
+
+puts "100 audit logs have been created"
